19b817dfa97e197971297ecff721d7e71a20b750
lazuardi <lazamediamxt@gmail.com>
2024-11-24 11:36:39 +0700

    firs

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Permission\Models\Role as ModelsRole;
 
-class Role extends ModelsRole
+class Role extends Role
 {
-    use HasFactory;
+    //
 }
